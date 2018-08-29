3fda2fb58bd9b7ee307f91e88961dba8007d3711
Sidnei Simmon <sidneisimmon@gmail.com>
2018-08-29 17:21:56 -0300

    Output Management

--- src/index.js
+++ src/index.js
@@ -1,20 +1,16 @@
  import _ from 'lodash';
- import "./style.css";
- import Icon from "./github-128.png";
- import Data from './data.xml';
+ import printMe from "./print.js";
 
  function component() {
      let element = document.createElement('div');
-
+     var btn = document.createElement('button');
+     
      element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-     element.classList.add('hello')
 
-     var myIcon = new Image();
-     myIcon.src = Icon;
+     btn.innerHTML = 'Click me and check the console!';
+     btn.onclick = printMe;
 
-     element.appendChild(myIcon);
-
-     console.log(Data);
+     element.appendChild(btn);
 
      return element;
  }
